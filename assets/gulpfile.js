5097e681e13e875bffaf3bb245a95e6bbeb9497e
tangmeng <silence.tang@btcc.com>
2018-08-18 18:47:22 +0800

    minify js

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -8,6 +8,10 @@ var paths = {
   styles: {
     src: 'css/style.css',
     dest: 'css/'
+  },
+  scripts: {
+    src: 'js/usejs/*.js',
+    dest: 'js/'
   }
 }
 function styles() {
@@ -20,7 +24,16 @@ function styles() {
     }))
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
 exports.styles = styles;
-var build = styles
+exports.scripts = scripts;
+var build = gulp.parallel(styles, scripts)
 gulp.task('build', build);
 gulp.task('default', build);
